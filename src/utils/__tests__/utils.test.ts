f7de9b3aefcc4ee7ca1739748151b9b340bd14e0
olehyaremyn7 <oleh_yaremyn@ukr.net>
2022-11-09 20:58:42 +0200

    Added unit tests for utils

--- src/utils/__tests__/utils.test.ts
+++ src/utils/__tests__/utils.test.ts
@@ -1,10 +1,22 @@
-import { createUID } from '../index';
+import { createUID, isArrayEmpty } from '../index';
 
 describe('Utils module', (): void => {
+  test('createUID() should return id', (): void => {
+    expect(createUID()).toBeTruthy();
+  });
+
   test('createUID() should return unique id', (): void => {
     const idOne: string = createUID();
     const idTwo: string = createUID();
 
     expect(idOne).not.toEqual(idTwo);
+  });
+
+  test('isArrayEmpty() should return true if array is empty', (): void => {
+    expect(isArrayEmpty([])).toBeTruthy();
+  });
+
+  test('isArrayEmpty() should return false if array is not empty', (): void => {
+    expect(isArrayEmpty([1, 2, 3, 4, 5])).toBeFalsy();
   });
 });
